3e7ca3f63c55465dbd12e4327c4bc36a8001f614
drull1000 <pcaetanop@protonmail.com>
2021-05-08 13:21:55 -0300

    Removing function

--- script.js
+++ script.js
@@ -2,12 +2,3 @@ const productButton = document.querySelector("#Product");
 const companyButton = document.querySelector("#Company");
 const connectButton = document.querySelector("#Connect");
 const productSuboption = document.querySelector(".productSuboption");
-
-//const productButton = document.querySelector("#product")
-
-function showProduct() {
-  productButton.addEventListener("click", () => {
-    productSuboption.style.display = "flex";
-  });
-}
-showProduct();
