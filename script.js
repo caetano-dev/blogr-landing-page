a896becef2860ccdf8cce305df7f2b57b15ed4b6
drull1000 <pcaetanop@protonmail.com>
2021-05-28 21:27:31 -0300

    Toggle function for image

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const product = document.querySelector(".productContainer");
 const company = document.querySelector(".companyContainer");
 const connect = document.querySelector(".connectContainer");
+const hamnavLabel = document.querySelector(".hamnavLabel");
 
-const toggle = () =>{
-    product.addEventListener("click", ()=>{
-        productList.classList.toggle("option-active")
-    })
-    company.addEventListener("click", ()=>{
-        companyList.classList.toggle("option-active")
-    })
-    connect.addEventListener("click", ()=>{
-        connectList.classList.toggle("option-active")
-    })
-}
+const toggle = () => {
+  hamnavLabel.addEventListener("click", () => {
+    hamnavLabel.classList.toggle("hamnavLabel-active");
+  });
+  product.addEventListener("click", () => {
+    productList.classList.toggle("option-active");
+  });
+  company.addEventListener("click", () => {
+    companyList.classList.toggle("option-active");
+  });
+  connect.addEventListener("click", () => {
+    connectList.classList.toggle("option-active");
+  });
+};
 toggle();
+
+window.addEventListener("resize", () => {
+  console.log(window.innerWidth);
+});
+
